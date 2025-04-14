74f7a403e27c7a66c1e0820580a5802856d5b424
oscar <oscar.rivera@adquintue.com>
2025-04-14 19:21:04 -0400

    pipeline

--- pipeline_status.sh
+++ pipeline_status.sh
@@ -12,8 +12,9 @@ while IFS='|' read -r line; do
   ALUMNO=${arrIN[2]}
 
   BADGE="[![Build](https://github.com/${REPO}/dsy1103-usuario/actions/workflows/compilacion.yml/badge.svg)](https://github.com/${REPO}/dsy1103-usuario/actions/workflows/compilacion.yml)"
+  BADGE_PRUEBAS="[![Build](https://github.com/${REPO}/dsy1103-usuario/actions/workflows/tests.yml/badge.svg)](https://github.com/${REPO}/dsy1103-usuario/actions/workflows/tests.yml)"
 
-  echo "|${ALUMNO}|${BADGE}|[${REPO}](https://github.com/${REPO}/dsy1103-usuario)" >> $FILE
+  echo "|${ALUMNO}|${BADGE}|${BADGE_PRUEBAS}|[${REPO}](https://github.com/${REPO}/dsy1103-usuario)" >> $FILE
 
 done < repos.txt
 
