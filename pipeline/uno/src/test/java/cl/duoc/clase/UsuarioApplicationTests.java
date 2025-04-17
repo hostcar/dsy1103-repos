d6b11171e5618f0b6a8e61dd8153f3d980903a0c
oscar <oscar.rivera@adquintue.com>
2025-04-17 13:35:32 -0400

    tests

---
+++ pipeline/uno/src/test/java/cl/duoc/clase/UsuarioApplicationTests.java
@@ -0,0 +1,12 @@
+package cl.duoc.clase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UsuarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
