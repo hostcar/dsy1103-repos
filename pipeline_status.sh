9038ae2e512ba931b18b40b4a9456344eec67656
oscar <oscar.rivera@adquintue.com>
2025-04-28 19:49:26 -0400

    biblioteca

--- pipeline_status.sh
+++ pipeline_status.sh
@@ -11,7 +11,7 @@ while IFS='|' read -r line; do
   REPO=${arrIN[1]}
   ALUMNO=${arrIN[2]}
 
-  BADGE="[![Build](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/compilacion.yml/badge.svg)](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/compilacion.yml)"
+  BADGE="[![Build](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/build.yml/badge.svg)](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/build.yml)"
   BADGE_PRUEBAS="[![Build](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/tests.yml/badge.svg)](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/tests.yml)"
 
   echo "|${ALUMNO}|${BADGE}|${BADGE_PRUEBAS}|[${REPO}](https://github.com/${REPO}/dsy1103-biblioteca)" >> $FILE
