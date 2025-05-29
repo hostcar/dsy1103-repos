12ea67ab08e949a45871a7ca62cfacc4304555ab
oscar <oscar.rivera@adquintue.com>
2025-05-28 20:13:51 -0400

    salas-bibliotecas build

--- pipeline_status.sh
+++ pipeline_status.sh
@@ -11,10 +11,10 @@ while IFS='|' read -r line; do
   REPO=${arrIN[1]}
   ALUMNO=${arrIN[2]}
 
-  BADGE="[![Build](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/build.yml/badge.svg)](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/build.yml)"
-  BADGE_PRUEBAS="[![Build](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/tests.yml/badge.svg)](https://github.com/${REPO}/dsy1103-biblioteca/actions/workflows/tests.yml)"
+  BADGE="[![Build](https://github.com/${REPO}/dsy1103-salas-bibliotecas/actions/workflows/build.yml/badge.svg)](https://github.com/${REPO}/dsy1103-salas-bibliotecas/actions/workflows/build.yml)"
+  BADGE_PRUEBAS="[![Build](https://github.com/${REPO}/dsy1103-salas-bibliotecas/actions/workflows/tests.yml/badge.svg)](https://github.com/${REPO}/dsy1103-salas-bibliotecas/actions/workflows/tests.yml)"
 
-  echo "|${ALUMNO}|${BADGE}|${BADGE_PRUEBAS}|[${REPO}](https://github.com/${REPO}/dsy1103-biblioteca)" >> $FILE
+  echo "|${ALUMNO}|${BADGE}|${BADGE_PRUEBAS}|[${REPO}](https://github.com/${REPO}/dsy1103-salas-bibliotecas)" >> $FILE
 
 done < repos.txt
 
