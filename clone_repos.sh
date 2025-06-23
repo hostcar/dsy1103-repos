a25c41ddec1e8d782a9e092fe1766639cc89449b
oscar <oscar.rivera@adquintue.com>
2025-06-23 19:45:05 -0400

    pipeline

--- clone_repos.sh
+++ clone_repos.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-PIPELINE_DIR=pipeline/dsy1103-for-books/
+PIPELINE_DIR=pipeline/dsy1103-for-users/
 
 if [ ! -d "alumnos" ]; then
     echo "alumnos does not exist."
@@ -12,7 +12,7 @@ while IFS='|' read -r line; do
 
   arrIN=(${(s[;])line})
   REPO=${arrIN[1]}
-  echo "https://github.com/${REPO}/dsy1103-for-books"
+  echo "https://github.com/${REPO}/dsy1103-for-users"
 
   DIRECTORY=alumnos/$REPO
   if [ -d "$DIRECTORY" ]; then
@@ -22,7 +22,7 @@ while IFS='|' read -r line; do
 
   mkdir $DIRECTORY
 
-  git clone --branch main git@github.com:${REPO}/dsy1103-for-books.git $DIRECTORY
+  git clone --branch main git@github.com:${REPO}/dsy1103-for-users.git $DIRECTORY
 
   pwd
   ls -l pipeline
